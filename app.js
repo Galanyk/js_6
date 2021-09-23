1d774bb4ceddded363a5dc9603240a673c604b23
Galanyksa1@gmail.com <Galanyksa1@gmail.com>
2021-09-23 20:36:44 +0300

    Code

---
+++ app.js
@@ -0,0 +1,40 @@
+const user = {
+    name: 'Bob',
+    age: 34,
+    id: 1,
+    address: {
+        city: 'Odessa',
+        country: 'Ukraine'
+    },
+
+    books: [{
+        name: 'red one',
+        id: 35,
+        author: {
+            name: 'Some Author',
+            id: 277
+        },
+        tags: ['history', 'roman']
+    }]
+}
+
+function recursiveFunc(obj, result = {}) {
+    for (let key in obj) {
+        if (typeof obj[key] !== 'object') {
+            result[key] = obj[key];
+        } else {
+            if (Array.isArray(obj[key])) {
+                result[key] = obj[key];
+            } else if (typeof obj[key] === 'object') {
+                result[key] = obj[key]
+            } else {
+                recursiveFunc(obj[key], result)
+            }
+        }
+    }
+    return result;
+}
+
+const foo = recursiveFunc(user);
+console.log(user);
+console.log(foo);
